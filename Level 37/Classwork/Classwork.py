c59a86e40344ce0d526885fbfbf4efe4155b065a
Irakli-12 <gegiairakli13@gmail.com>
2025-07-02 21:21:49 +0400

    second commit

---
+++ Level 37/Classwork/Classwork.py
@@ -0,0 +1,19 @@
+# https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/train/python
+# def quarter_of(month):
+#     if month in range(1, 4):
+#         return 1
+#     elif month in range(4, 7):
+#         return 2
+#     elif month in range(7, 10):
+#         return 3
+#     else:
+#         return 4
+# https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/train/python
+# def find_average(numbers):
+#     if not numbers:
+#         return 0
+#     else:
+#         return sum(numbers) / len(numbers)
+# https://www.codewars.com/kata/57a0885cbb9944e24c00008e/train/python
+# def remove_exclamation_marks(s):
+    # return s.replace("!", "")
---
+++ Level 37/Homework/Homework.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
+# def accum(st):
+    # return '-'.join((st * (i + 1)).capitalize() for i, st in enumerate(st))
+# https://www.codewars.com/kata/5467e4d82edf8bbf40000155
+# https://www.codewars.com/kata/54c27a33fb7da0db0100040e
+# https://www.codewars.com/kata/54ff3102c1bad923760001f3
+# def get_count(sentence):
+#     vowels = "aeiou"
+#     count = 0
+#     for x in sentence:
+#         if x in vowels:
+#             count += 1
+#     return count
+# https://www.codewars.com/kata/56269eb78ad2e4ced1000013
